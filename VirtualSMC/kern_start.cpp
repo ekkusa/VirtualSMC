06a71592bcca35d897d0acd125cd057cd34eede2
exer <98556122+ekkusa@users.noreply.github.com>
2024-07-03 15:00:20 +0800

    idk

--- VirtualSMC/kern_start.cpp
+++ VirtualSMC/kern_start.cpp
@@ -32,7 +32,7 @@ PluginConfiguration ADDPR(config) {
 	arrsize(bootargDebug),
 	bootargBeta,
 	arrsize(bootargBeta),
-	KernelVersion::MountainLion,
+	KernelVersion::HighSierra,
 	KernelVersion::Sequoia,
 	[]() {
 		auto prov = VirtualSMCProvider::getInstance();
